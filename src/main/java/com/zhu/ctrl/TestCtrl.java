96c04af0c2f4f057e97daf8a86ef60fed92a5814
zhu <983132370@qq.com>
2016-12-27 17:57:37 +0800

    添加个show love 页面.

--- src/main/java/com/zhu/ctrl/TestCtrl.java
+++ src/main/java/com/zhu/ctrl/TestCtrl.java
@@ -1,5 +1,6 @@
 package com.zhu.ctrl;
 import com.zhu.biz.UserBiz;
+import com.zhu.context.RequestContext;
 import com.zhu.util.JsonUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by zhu on 2016/11/24.
@@ -20,10 +24,17 @@ public class TestCtrl {
     @Autowired
     UserBiz userBiz;
 
+    @RequestMapping(value = {"/"}, method= RequestMethod.GET)
+    public String testLove(){
+        log.debug(RequestContext.getClientIp());
+
+        return "cavanlove/love";
+    }
     //返回json
     @ResponseBody
     @RequestMapping(value = {"/testMongo"}, method= RequestMethod.GET)
     public String testMongo(){
+        log.debug(RequestContext.getClientIp());
         return JsonUtil.toString(userBiz.testUser());
     }
 
